a03e11751a76563e5c97e94907faa7028c64b7de
George Krachtopoulos <georgekraxt@gmail.com>
2025-03-05 02:20:38 +0200

    fixed chromium executable path

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const puppeteer = require('puppeteer-core');
 const chromium = require('@sparticuz/chromium');
@@ -8,10 +9,13 @@ const app = express();
 const port = 3000;
 
 (async () => {
+  let executablePath = process.env.CHROME_EXECUTABLE_PATH;
+  if (!executablePath) executablePath = await chromium.executablePath();
+
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
+    executablePath,
     headless: chromium.headless,
   });
   
